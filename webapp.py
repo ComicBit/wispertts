a965c6a59d57b57d0098a5b989c408ad2b854606
Leo Piccione 🌋 <leandro.piccione.ext@sisal.it>
2025-06-25 18:18:41 +0200

    Definition of port

--- webapp.py
+++ webapp.py
@@ -92,4 +92,4 @@ def download(job_id):
     return send_file(job.outfile, as_attachment=True, download_name='transcript.txt')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8888)
